3b6a0ea1982cc13fd79fb0e7542f2816ab7ec465
Iulian Margarintescu <etishor@gmail.com>
2011-04-08 15:23:53 +0300

    Add CustomObjectProperty

---
+++ SerializersTests/Messages/CustomObjectProperty.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using ProtoBuf;
+using MbUnit.Framework;
+
+namespace SerializersTests.Messages
+{
+    [Serializable]
+    [DataContract]
+    [ProtoContract]
+    public class CustomObject
+    {
+        [DataMember]
+        [ProtoMember(1)]
+        public int Value { get; set; }
+    }
+
+    [Serializable]
+    [DataContract]
+    [ProtoContract]
+    public class CustomObjectProperty : IAssertEquality
+    {
+        [DataMember]
+        [ProtoMember(1)]
+        private CustomObject Value { get; set; }
+
+        public static CustomObjectProperty CreateInstance()
+        {
+            return new CustomObjectProperty { Value = new CustomObject { Value = 10 } };
+        }
+
+        public void AssertEquality(object other)
+        {
+            Assert.IsNotNull(other);
+            Assert.IsInstanceOfType<CustomObjectProperty>(other);
+            CustomObjectProperty target = other as CustomObjectProperty;
+
+            Assert.AreNotSame(this.Value, target.Value);
+            Assert.IsNotNull(this.Value);
+            Assert.IsNotNull(target.Value);
+            Assert.AreEqual(this.Value.Value, target.Value.Value);
+        }
+    }
+}
