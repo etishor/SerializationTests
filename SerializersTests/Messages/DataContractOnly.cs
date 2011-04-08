f902cc4e988e0940d851af79e43097ecfd2b6e30
Iulian Margarintescu <etishor@gmail.com>
2011-04-08 16:42:01 +0300

    Add DataContractOnly and SerializableOnly

---
+++ SerializersTests/Messages/DataContractOnly.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using System.Runtime.Serialization;
+
+namespace SerializersTests.Messages
+{
+    [DataContract]
+    public class DataContractOnly : IAssertEquality
+    {
+        [DataMember]
+        public int ValueField;
+
+        [DataMember]
+        public int ValueProperty { get; set; }
+
+        public static DataContractOnly CreateInstance()
+        {
+            return new DataContractOnly { ValueField = 10, ValueProperty = 20 };
+        }
+
+        public void AssertEquality(object other)
+        {
+            Assert.IsNotNull(other);
+            Assert.IsInstanceOfType<DataContractOnly>(other);
+            DataContractOnly target = other as DataContractOnly;
+
+            Assert.AreEqual(this.ValueField, target.ValueField);
+            Assert.AreEqual(this.ValueProperty, target.ValueProperty);
+        }
+    }
+}
---
+++ SerializersTests/Messages/SerializableOnly.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+
+namespace SerializersTests.Messages
+{
+    [Serializable]
+    public class SerializableOnly : IAssertEquality
+    {
+        public int ValueField;
+
+        public int ValueProperty { get; set; }
+
+        public static SerializableOnly CreateInstance()
+        {
+            return new SerializableOnly { ValueField = 10 , ValueProperty = 20 };
+        }
+
+        public void AssertEquality(object other)
+        {
+            Assert.IsNotNull(other);
+            Assert.IsInstanceOfType<SerializableOnly>(other);
+            SerializableOnly target = other as SerializableOnly;
+
+            Assert.AreEqual(this.ValueField, target.ValueField);
+            Assert.AreEqual(this.ValueProperty, target.ValueProperty);
+        }
+    }
+}
