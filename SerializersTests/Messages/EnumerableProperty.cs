3870b4aa796c65022cb184c907fd13f6a9bc12ab
Iulian Margarintescu <etishor@gmail.com>
2011-04-08 15:18:58 +0300

    Add EnumerableProperty

---
+++ SerializersTests/Messages/EnumerableProperty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using ProtoBuf;
+using MbUnit.Framework;
+
+namespace SerializersTests.Messages
+{
+    [Serializable]
+    [DataContract]
+    [ProtoContract]
+    public class EnumerableProperty : IAssertEquality
+    {
+        [DataMember]
+        [ProtoMember(1)]
+        private IEnumerable<int> Value { get; set; }
+
+        public static EnumerableProperty CreateInstance()
+        {
+            return new EnumerableProperty { Value = new int[] { 10, 20 } };
+        }
+
+        public void AssertEquality(object other)
+        {
+            Assert.IsNotNull(other);
+            Assert.IsInstanceOfType<EnumerableProperty>(other);
+            EnumerableProperty target = other as EnumerableProperty;
+
+            Assert.AreElementsEqual(this.Value, target.Value);
+        }
+    }
+}
