eb321a220bb1882818576ebbd91556f6cd711e55
Iulian Margarintescu <etishor@gmail.com>
2011-05-03 12:27:25 +0300

    Add Raven.Json serialization

---
+++ SerializersTests/Adapters/RavenJsonAdapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace SerializersTests.Adapters
+{
+    public class RavenJsonAdapter : ISerializerAdapter
+    {
+        private readonly JsonSerializer serializer = new JsonSerializer();
+
+        public void Serialize<T>(System.IO.Stream stream, T instance)
+        {
+            var o = RavenJObject.FromObject(instance);
+            
+            using(var text = new StreamWriter(new IndisposableStream(stream)))
+            {
+                using(var jsonWriter = new JsonTextWriter(text))
+                {
+                    o.WriteTo(jsonWriter);
+                }
+            }
+        }
+
+        public T Deserialize<T>(System.IO.Stream stream)
+        {
+            using (var text = new StreamReader(new IndisposableStream(stream)))
+            {
+                using(var jsonReader = new JsonTextReader(text))
+                {
+                    var jobject = RavenJObject.ReadFrom(jsonReader);
+
+                    return serializer.Deserialize<T>(new RavenJTokenReader(jobject));
+                }
+            }
+        }
+    }
+}
