e6dc668c0a9fbeec033fb83e250916913a7aa918
Iulian Margarintescu <etishor@gmail.com>
2011-05-03 12:27:11 +0300

    Add sealed classes

---
+++ SerializersTests/Messages/SealedPublicReadOnlyNamesInConstructor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using System.Runtime.Serialization;
+using ProtoBuf;
+
+namespace SerializersTests.Messages
+{
+    [Serializable]
+    [DataContract]
+    [ProtoContract]
+    public sealed class SealedPublicReadOnlyNamesInConstructor : IAssertEquality
+    {
+        [DataMember]
+        [ProtoMember(1)]
+        public readonly int IntValue;
+
+        public SealedPublicReadOnlyNamesInConstructor(int intValue)
+        {
+            this.IntValue = intValue;
+        }
+
+        public static SealedPublicReadOnlyNamesInConstructor CreateInstance()
+        {
+            return new SealedPublicReadOnlyNamesInConstructor(10);
+        }
+
+        public void AssertEquality(object other)
+        {
+            Assert.IsNotNull(other);
+            Assert.IsInstanceOfType<SealedPublicReadOnlyNamesInConstructor>(other);
+
+            SealedPublicReadOnlyNamesInConstructor target = other as SealedPublicReadOnlyNamesInConstructor;
+
+            Assert.AreEqual(this.IntValue, target.IntValue);            
+        }
+    }
+}
---
+++ SerializersTests/Messages/SealedPublicReadOnlyNamesInConstructorWithPrivateParameterlessConstructor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using System.Runtime.Serialization;
+using ProtoBuf;
+
+namespace SerializersTests.Messages
+{
+    [Serializable]
+    [DataContract]
+    [ProtoContract]
+    public sealed class SealedPublicReadOnlyNamesInConstructorWithPrivateParameterlessConstructor : IAssertEquality
+    {
+        [DataMember]
+        [ProtoMember(1)]
+        public readonly int IntValue;
+
+        private SealedPublicReadOnlyNamesInConstructorWithPrivateParameterlessConstructor() {}
+
+        public SealedPublicReadOnlyNamesInConstructorWithPrivateParameterlessConstructor(int intValue)
+        {
+            this.IntValue = intValue;
+        }
+
+        public static SealedPublicReadOnlyNamesInConstructorWithPrivateParameterlessConstructor CreateInstance()
+        {
+            return new SealedPublicReadOnlyNamesInConstructorWithPrivateParameterlessConstructor(10);
+        }
+
+        public void AssertEquality(object other)
+        {
+            Assert.IsNotNull(other);
+            Assert.IsInstanceOfType<SealedPublicReadOnlyNamesInConstructorWithPrivateParameterlessConstructor>(other);
+
+            SealedPublicReadOnlyNamesInConstructorWithPrivateParameterlessConstructor target = other as SealedPublicReadOnlyNamesInConstructorWithPrivateParameterlessConstructor;
+
+            Assert.AreEqual(this.IntValue, target.IntValue);            
+        }
+    }
+}
