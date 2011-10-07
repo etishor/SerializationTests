973b213ff93ac8645442b5abbd6d29b5e1f56b58
Iulian Margarintescu <etishor@gmail.com>
2011-10-07 16:19:11 +0300

    update servicestack & add more uri tests

---
+++ SerializersTests/Messages/UriWithQuotesAndSlashes.cs
@@ -0,0 +1,41 @@
+﻿
+namespace SerializersTests.Messages
+{
+    using System;
+    using System.Runtime.Serialization;
+    using MbUnit.Framework;
+    using ProtoBuf;
+
+    [Serializable]
+    [DataContract]
+    [ProtoContract]
+    public class UriWithQuotesAndSlashes : IAssertEquality
+    {
+        [DataMember]
+        [ProtoMember(1)]
+        public Uri ValueWithQuotes { get; set; }
+
+        [DataMember]
+        [ProtoMember(2)]
+        public Uri ValueWithSlashes { get; set; }
+
+        public static UriWithQuotesAndSlashes CreateInstance()
+        {
+            return new UriWithQuotesAndSlashes
+            {
+                ValueWithQuotes = new Uri("http://test.com/%22foo+bar%22"),
+                ValueWithSlashes = new Uri(@"http://tes/?a=b\\c&d=e\")
+            };
+        }
+
+        public void AssertEquality(object other)
+        {
+            Assert.IsNotNull(other);
+            Assert.IsInstanceOfType<UriWithQuotesAndSlashes>(other);
+            UriWithQuotesAndSlashes target = other as UriWithQuotesAndSlashes;
+
+            Assert.AreEqual(this.ValueWithQuotes.ToString(), target.ValueWithQuotes.ToString());
+            Assert.AreEqual(this.ValueWithSlashes.ToString(), target.ValueWithSlashes.ToString());
+        }
+    }
+}
