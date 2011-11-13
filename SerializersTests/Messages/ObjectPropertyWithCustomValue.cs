2498d42b1eea65759838a729bf5f9b1c41c34bd6
Iulian Margarintescu <etishor@gmail.com>
2011-11-13 15:51:47 +0200

    Update raven json adapter to latest version, also add tests for ravendb store & load

--- SerializersTests/Messages/ObjectPropertyWithCustomValue.cs
+++ SerializersTests/Messages/ObjectPropertyWithCustomValue.cs
@@ -1,36 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.Serialization;
-using ProtoBuf;
 using MbUnit.Framework;
+using ProtoBuf;
 
 namespace SerializersTests.Messages
 {
-    [Serializable]
-    [DataContract]
-    [ProtoContract]
-    public class ObjectPropertyWithCustomValue : IAssertEquality
-    {
-        [DataMember]
-        [ProtoMember(1)]
-        private object Value {get;set;}
+	[Serializable]
+	[DataContract]
+	[ProtoContract]
+	public class ObjectPropertyWithCustomValue : IAssertEquality
+	{
+		[DataMember]
+		[ProtoMember(1)]
+		private object Value {get;set;}
 
-        public static ObjectPropertyWithCustomValue CreateInstance()
-        {
-            //return new ObjectPropertyWithCustomValue { Value = new CustomObject() { Value = 10 } };
-            return new ObjectPropertyWithCustomValue { Value = new Dictionary<int, List<string>> { } };
-        }
+		public static ObjectPropertyWithCustomValue CreateInstance()
+		{
+			return new ObjectPropertyWithCustomValue { Value = new CustomObject() { Value = 10 } };
+		}
 
-        public void AssertEquality(object other)
-        {
-            Assert.IsNotNull(other);
-            Assert.IsInstanceOfType<ObjectPropertyWithCustomValue>(other);
-            ObjectPropertyWithCustomValue target = other as ObjectPropertyWithCustomValue;
+		public void AssertEquality(object other)
+		{
+			Assert.IsNotNull(other);
+			Assert.IsInstanceOfType<ObjectPropertyWithCustomValue>(other);
+			ObjectPropertyWithCustomValue target = other as ObjectPropertyWithCustomValue;
 
-            Assert.IsInstanceOfType(this.Value.GetType(), target.Value);
-            //Assert.AreEqual(((CustomObject)this.Value).Value, ((CustomObject)target.Value).Value);
-        }
-    }
+			CustomObject val = target.Value as CustomObject;
+			Assert.IsNotNull(val);
+			Assert.AreEqual(10, val.Value);
+		}
+	}
 }
---
+++ SerializersTests/RavenDbTests.cs
@@ -0,0 +1,56 @@
+﻿namespace SerializersTests
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Reflection;
+	using MbUnit.Framework;
+	using Raven.Tests;
+	
+	public class RavenDbTests : LocalClientTest
+	{
+		[StaticTestFactory]
+		public static IEnumerable<Test> CreateTests()
+		{
+			TestSuite test = new TestSuite(@"Raven Tests");
+
+			foreach (Type message in SerializationTests.GetMessages())
+			{
+				test.Children.Add(BuildTestCase(message));
+			}
+
+			yield return test;
+		}
+
+		private static Test BuildTestCase(Type message)
+		{
+			string name = string.Format("RavenDb_{0}", message.Name);
+			TestCase test = new TestCase(name, () => new RavenDbTests().RunTest(message));
+			return test;
+		}
+
+		private void RunTest(Type messageType)
+		{
+			IAssertEquality message = (IAssertEquality)messageType.GetMethod("CreateInstance",
+				BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy).Invoke(null, null);
+
+
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Store(message, "test");
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+
+					var result = session.Load<IAssertEquality>("test");
+					message.AssertEquality(result);
+				}
+			}
+
+		}
+
+	}
+}
