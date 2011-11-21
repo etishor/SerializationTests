185259b85563befcf9aea607f2eb09552d637450
Iulian Margarintescu <etishor@gmail.com>
2011-11-21 15:22:18 +0200

    Fix bench

--- Serialization.Bench/Program.cs
+++ Serialization.Bench/Program.cs
@@ -6,6 +6,8 @@ using System.Runtime.Serialization.Formatters.Binary;
 using SerializersTests;
 using SerializersTests.Messages;
 using System.Text;
+using System.Reflection;
+using Newtonsoft.Json.Serialization;
 
 namespace Serialization.Bench
 {
@@ -13,6 +15,16 @@ namespace Serialization.Bench
     {
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += (s, o) =>
+            {
+                var name = new AssemblyName(o.Name);
+                if (name.Name.Contains("Newtonsoft.Json"))
+                {
+                    return typeof(JsonContract).Assembly;
+                }
+                return null;
+            };
+
             SerializationBenchmark.Run();
 
             Console.WriteLine("Done");
